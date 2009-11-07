785a4695bcd20daf83803705808f8305686ef72e
Jonathan Ko <jonathan.ko@berkeley.edu>
2009-11-07 21:00:32 +0000

    Capified
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@11 3207efe2-bddd-11de-9616-7b1d4728551e

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+# Replace with the HTTP (NOT HTTPS) read-only URL of your Google Code SVN.
+set :repository, "http://research-cs194.googlecode.com/svn/trunk/"
+
+# Directory for deployment on the production (remote) machine.
+set :deploy_to, "/mnt/app"
+
+# Replace the below with the hostname of your EC2 instance.
+set :machine_name, "ec2-174-129-122-92.compute-1.amazonaws.com"
+
+# We're using one instance for all three roles.
+role :app, "#{machine_name}"
+role :web, "#{machine_name}"
+role :db,  "#{machine_name}", :primary => true
+
+set :use_sudo, false
+set :user, "root"
+
+ssh_options[:keys] = File.join(ENV["HOME"], "/id_rsa-cs194")
+
+namespace :deploy do
+  %w(start stop restart).each do |action| 
+     desc "#{action} the Thin processes"  
+     task action.to_sym do
+       find_and_execute_task("thin:#{action}")
+    end
+  end 
+end
+
+namespace :thin do  
+  %w(start stop restart).each do |action| 
+  desc "#{action} the app's Thin Cluster"  
+    task action.to_sym, :roles => :app do  
+      run "thin #{action} -c #{deploy_to}/current -e production -p 80" 
+    end
+  end
+end
