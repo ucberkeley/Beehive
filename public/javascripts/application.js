338e266623bd1c7bc5a89c41141d0111cc471737
Amber Feng <amber.feng@gmail.com>
2009-11-26 01:09:31 +0000

    JS Button hovers
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@76 3207efe2-bddd-11de-9616-7b1d4728551e

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function menu_bar_init(){
+	//Menu Bar Button Hover
+	$("menu_bar_01").observe('mouseout', function() {
+	  this.src = "../images/menu_bar_fade_01.png";
+	});
+	$("menu_bar_01").observe('mouseover', function() {
+	  this.src = "../images/menu_bar_01.png";
+	});
+
+	$("menu_bar_02").observe('mouseout', function() {
+	  this.src = "../images/menu_bar_fade_02.png";
+	});
+	$("menu_bar_02").observe('mouseover', function() {
+	  this.src = "../images/menu_bar_02.png";
+	});
+
+	$("menu_bar_03").observe('mouseout', function() {
+	  this.src = "../images/menu_bar_fade_03.png";
+	});
+	$("menu_bar_03").observe('mouseover', function() {
+	  this.src = "../images/menu_bar_03.png";
+	});
+}
