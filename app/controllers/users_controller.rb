5c42218f535639d00224b81299f72043c95c9985
Amber Feng <amber.feng@gmail.com>
2009-11-27 07:35:54 +0000

    something WEIRD is happening, committing because i want to rollback
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@90 3207efe2-bddd-11de-9616-7b1d4728551e

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,10 +73,12 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-
+	
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
+	    puts "****************************"
+		puts params[:user][:name].to_s
         flash[:notice] = 'user was successfully updated.'
         format.html { redirect_to :controller => :dashboard }
         format.xml  { head :ok }
