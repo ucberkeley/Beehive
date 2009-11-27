b7af8d24a8f8acf2fe0c18e4c36db1fef27a6f21
Oren Blasberg <oblasberg@gmail.com>
2009-11-27 21:23:04 +0000

    Editing jobs now shows the current faculty. However, changing the faculty member currently does not update when you submit the change! (Title, desc, etc are fine, but faculty does not update)
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@102 3207efe2-bddd-11de-9616-7b1d4728551e

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091127022100) do
+ActiveRecord::Schema.define(:version => 20091127115519) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
