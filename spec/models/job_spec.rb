d5f509c82b95ee4e6404e2525b36f6110ac65867
Oren Blasberg <oblasberg@gmail.com>
2009-11-30 03:14:43 +0000

    Added just a few rspec tests to job_spec
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@152 3207efe2-bddd-11de-9616-7b1d4728551e

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -7,7 +7,7 @@ describe Job do
       :title => "value for title",
       :desc => "value for desc",
       :category_id => 1,
-      :exp_date => Time.now,
+      :exp_date => Time.now - 5.hours,
       :num_positions => 1,
       :paid => false,
       :credit => false,
@@ -24,6 +24,42 @@ describe Job do
 	job.sponsorships.stub!(:size).and_return(1)
 	puts job.errors
 	job.save.should be_true
+  end
+  
+  describe "title of the job" do
+	describe 'allows title of length >= 10 characters' do
+		["Awesome job. Really.", "ThisJobTEN", "Robotics research", 
+		"cool stuff found here"].each do |title_str|
+			it "'#{title_str}'" do
+				lambda do
+					j = create_job(:title => title_str)
+					j.errors.on(:title).should be_nil
+				end.should change(Job, :count).by(1)
+			end
+		end
+	end
+	
+  
+    describe 'disallows title of length < 10 characters' do
+		['Test', '', 'title', 'lol', 'fail'].each do |title_str|
+			it "'#{title_str}'" do
+				lambda do
+					j = create_job(:title => title_str)
+					j.errors.on(:title).should_not be_nil
+				end.should_not change(Job, :count)
+			end
+		end
+	end
+  end
+  
+
+  
+  protected
+  def create_job(options = {})
+    record = Job.new({ :title => 'SampleJobTitle', :desc => 'descriptiongoeshere', 
+		:exp_date => Time.now - 5.hours, :num_positions => 0 }.merge(options))
+    record.save
+    record
   end
   
 end
