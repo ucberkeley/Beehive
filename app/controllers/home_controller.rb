a3cccf2167c4d68d4cf82b25020c9d930d63e423
Oren Blasberg <oblasberg@gmail.com>
2009-11-30 09:11:10 +0000

    researchmatch logo at top of pages links to the actual home page now. Need to do: after user logs in, the first page s/he sees is the dashboard.
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@172 3207efe2-bddd-11de-9616-7b1d4728551e

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,7 @@
 class HomeController < ApplicationController
-	before_filter :redirect_if_logged_in
-	
+
 	def index
 		@departments = Department.all
 	end
 	
-	def redirect_if_logged_in
-		if logged_in?
-			redirect_to :controller=>:dashboard
-		end
-	end
 end
