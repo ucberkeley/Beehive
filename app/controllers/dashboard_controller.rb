8de729cabce31a51f76f31347a2c492666bdea29
Amber Feng <amber.feng@gmail.com>
2009-12-02 07:40:24 +0000

    limiting # smartmatches to 4
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@206 3207efe2-bddd-11de-9616-7b1d4728551e

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
   def index
 	@departments = Department.all
 	@recently_added_jobs = Job.find(:all, :conditions => [ "active = ?", true], :order => "created_at DESC", :limit => 5 )
-	@relevant_jobs = smartmatches_for current_user
+	@relevant_jobs = smartmatches_for(current_user)[0..3]
 	
 	watched_jobs = current_user.watches
 	@watched_jobs = []
