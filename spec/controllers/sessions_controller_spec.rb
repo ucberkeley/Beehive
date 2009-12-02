8c41f84806fd21f1d4c4cabc32f441d4c51af90c
Amber Feng <amber.feng@gmail.com>
2009-12-02 07:48:55 +0000

    sessions_controller_spec.rb fixed
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@207 3207efe2-bddd-11de-9616-7b1d4728551e

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -47,7 +47,7 @@ describe SessionsController do
             it "greets me nicely"            do do_create; response.flash[:notice].should =~ /success/i   end
             it "sets/resets/expires cookie"  do controller.should_receive(:handle_remember_cookie!).with(want_remember_me); do_create end
             it "sends a cookie"              do controller.should_receive(:send_remember_cookie!);  do_create end
-            it 'redirects to the home page'  do do_create; response.should redirect_to('/')   end
+            it 'redirects to the home page'  do do_create; response.should redirect_to(:controller=>:dashboard)   end
             it "does not reset my session"   do controller.should_not_receive(:reset_session).and_return nil; do_create end # change if you uncomment the reset_session path
             if (has_request_token == :valid)
               it 'does not make new token'   do @user.should_not_receive(:remember_me);   do_create end
