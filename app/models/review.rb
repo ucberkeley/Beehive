5694e860060757c893107e2988cd99b361c170aa
Oren Blasberg <oblasberg@gmail.com>
2010-03-19 07:13:36 +0000

    Added some basic validations to reviews; also put those ratings icons in the reviews index page. That page needs to be improved graphically though.
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@240 3207efe2-bddd-11de-9616-7b1d4728551e

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,10 @@ class Review < ActiveRecord::Base
 	belongs_to :faculty
 	
 	# validations
-	#validates_presence_of :
+	validates_presence_of :faculty, :body, :rating
+	validates_length_of :body, :minimum => 50
+	validates_numericality_of :rating
+	
 	
   
 end
