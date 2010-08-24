a1ad617b911c0094714c9c6bdc5e9a937d74a8ea
Jonathan Ko <jonathan.ko@berkeley.edu>
2010-08-24 22:45:43 +0000

    i dont know why this didn't commit before
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@268 3207efe2-bddd-11de-9616-7b1d4728551e

--- config/environment.rb
+++ config/environment.rb
@@ -38,4 +38,13 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+  
 end
+
+  # This is the root url for our app (like localhost:3000/)
+  # WITH trailing slash
+  $rm_root = "http://localhost:3000/"
+  
+  # Set up ActionMailer
+  ActionMailer::Base.default_url_options[:host] = $rm_root
+  ActionMailer::Base.delivery_method = :test
