3ed55469431d4ab273fa309ffd9048dff7cd606d
Jonathan Ko <jonathan.ko@berkeley.edu>
2010-11-10 05:12:43 +0000

    Fixed logout redirecting to absolute root.
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@368 3207efe2-bddd-11de-9616-7b1d4728551e

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -31,7 +31,7 @@ class SessionsController < ApplicationController
   def destroy
     logout_killing_session!
     flash[:notice] = "You have been logged out."
-    redirect_back_or_default('/')
+    redirect_back_or_default(:controller=>"dashboard", :action=>:index) 
   end
 
 protected
