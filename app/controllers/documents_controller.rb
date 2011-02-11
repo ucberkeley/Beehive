704504f8a0af238bd2ebfeac6b014b5b1285865f
Jonathan Ko <jonathan.ko@berkeley.edu>
2011-02-11 18:50:17 +0000

    Moving Rubycas client back to local plugin since its github svn seems to be down.
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@457 3207efe2-bddd-11de-9616-7b1d4728551e

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,8 +3,9 @@ class DocumentsController < ApplicationController
 include CASControllerIncludes
 
 #CalNet / CAS Authentication
-before_filter CASClient::Frameworks::Rails::Filter
+#before_filter CASClient::Frameworks::Rails::Filter
 #before_filter :setup_cas_user  
+before_filter :goto_cas_unless_logged_in
 before_filter :rm_login_required
 
 def new
