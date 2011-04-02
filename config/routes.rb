ff9dba52228c6b986daa00e9b8be2f34c5f14d0e
Oren Blasberg <oblasberg@gmail.com>
2011-04-02 15:53:41 -0700

    Authlogic works yay.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ ResearchMatch::Application.routes.draw do
 
   resources :users
 
+  resources :user_sessions
+  
+  match 'login' => "user_sessions#new",      :as => :login
+  match 'logout' => "user_sessions#destroy", :as => :logout
+
   root :to => "home#index"
 
   # The priority is based upon order of creation:
