4bbcf0b85ddfe6aa22687916215ac0e0216bc9cb
Oren Blasberg <oblasberg@gmail.com>
2011-04-02 17:08:11 -0700

    Added a spec

---
+++ spec/support/spec_helper_methods.rb
@@ -0,0 +1,7 @@
+class SpecHelperMethods
+  def self.stub_cas_ok  
+          # means either it logged in or something else happened that 
+          # the plugin deemed okay
+    CASClient::Frameworks::Rails::Filter.stub(:filter).and_return(true)
+  end
+end
