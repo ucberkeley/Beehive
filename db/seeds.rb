f2a13493d425f83babe5f69d79f85fff93a60c1f
Jonathan Ko <jonathanko@berkeley.edu>
2011-04-12 13:52:21 -0700

    Added eecs & test faculty seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+# Create departments
+[ ['Electrical Engineering and Computer Science', 'EECS'] ].each do |name, abbrev|
+  Department.find_or_create_by_name_and_abbrev(name, abbrev)
+end
+
+#   Development-specific seeds
+if Rails.env.eql? 'development' then
+  [ ['Test Faculty', 'test@faculty.com']
+  ].each do |name, email|
+    f = Faculty.find_or_initialize_by_name_and_email(name, email)
+    f.department_id = Department.first
+    f.save
+  end
+end
+
