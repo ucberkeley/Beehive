9973da8db7834acf291ee95af54615b6c7c352fa
Jonathan Ko <jonathanko@berkeley.edu>
2011-04-12 17:29:39 -0700

    Some durability changes to user session handling, and added description of user role on profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,7 @@ class ApplicationController < ActionController::Base
 
     def redirect_back_or_default(default)
       redirect_to(session[:return_to] || default)
-      session[:return_to] = nil
+      session.delete(:return_to)
+      #session[:return_to] = nil
     end
 end
