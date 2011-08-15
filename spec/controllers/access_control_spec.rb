e27bc41fa7e7eaa715de0d1786078457e932cf69
Oren Blasberg <oblasberg@gmail.com>
2011-08-15 13:07:44 -0700

    Moving to RSpec 2 & removed Review model

--- spec/controllers/access_control_spec.rb
+++ spec/controllers/access_control_spec.rb
@@ -1,8 +1,4 @@
 require File.dirname(__FILE__) + '/../spec_helper'
-  # Be sure to include AuthenticatedTestHelper in spec/spec_helper.rb instead
-# Then, you can remove it from this and the units test.
-include AuthenticatedTestHelper
-include AuthenticatedSystem
 
 #
 # A test controller with and without access controls
