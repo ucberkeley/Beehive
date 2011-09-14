6815743274687f46382de3dbc3715c201d81cc8d
Oren Blasberg <oblasberg@gmail.com>
2011-09-13 21:29:04 -0700

    Mailer allows env variable option now for smtp_settings

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -2,6 +2,10 @@
 settings_path = File.join Rails.root, 'config', 'smtp_settings'
 begin
   require settings_path
+
+  $smtp_username ||= ENV['SMTP_USERNAME']
+  $smtp_password ||= ENV['SMTP_PASSWORD']
+
   ResearchMatch::Application.configure do
     ActionMailer::Base.smtp_settings = {
       :address              => "smtp.gmail.com",
