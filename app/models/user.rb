dd51df89a8677d64e6c32925e66308242ebd42e5
Jonathan Ko <jonathan.ko@berkeley.edu>
2011-09-28 22:52:43 -0700

    Fix for user name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,7 +61,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of     :name
   validates_length_of       :name,     :within => 0..100
-  validates_format_of       :name,     :with => /\A[A-Za-z\-_ ]+\z/
+  validates_format_of       :name,     :with => /\A[A-Za-z\-_ \.]+\z/
 
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
