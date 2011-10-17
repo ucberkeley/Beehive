22ae8392705e63b3461d85b273645b00d5a666ec
Jonathan Ko <jonathan.ko@berkeley.edu>
2011-10-16 20:29:51 -0700

    Fix for initializers/action_mailer env vars
    Initializer tries env vars, then settings file

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,23 +1,26 @@
 # Configure the Action Mailer.
 settings_path = File.join Rails.root, 'config', 'smtp_settings'
-begin
-  require settings_path
 
-  $smtp_username ||= ENV['SMTP_USERNAME']
-  $smtp_password ||= ENV['SMTP_PASSWORD']
+$smtp_username ||= ENV['SMTP_USERNAME']
+$smtp_password ||= ENV['SMTP_PASSWORD']
 
-  ResearchMatch::Application.configure do
-    ActionMailer::Base.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :domain               => 'researchmatch.heroku.com',
-      :user_name            => $smtp_username,
-      :password             => $smtp_pw,
-      :authentication       => 'plain',
-      :enable_starttls_auto => true
-    }
+unless $smtp_username and $smtp_password
+  begin
+    require settings_path
+  rescue LoadError
+    $stderr.puts "Warning: Could not determine ActionMailer settings: see https://github.com/jonathank/ResearchMatch/wiki/Usage-Notes-(IMPORTANT)"
   end
-rescue LoadError
-  $stderr.puts "Warning: Could not load #{settings_path}.rb: see https://github.com/jonathank/ResearchMatch/wiki/Usage-Notes-(IMPORTANT)"
+end
+
+ResearchMatch::Application.configure do
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'researchmatch.heroku.com',
+    :user_name            => $smtp_username,
+    :password             => $smtp_pw,
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
 end
 
