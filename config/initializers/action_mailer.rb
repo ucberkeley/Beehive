bf726e86c73e7789b496d2a82af8d3e6c7a9c03f
Jonathan Ko <jonathan.ko@berkeley.edu>
2011-11-08 01:42:30 -0800

    Fix for actionmailer

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -16,10 +16,10 @@ ResearchMatch::Application.configure do
   ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'researchmatch.heroku.com',
+    :domain               => $smtp_username,
     :user_name            => $smtp_username,
-    :password             => $smtp_pw,
-    :authentication       => 'plain',
+    :password             => $smtp_password,
+    :authentication       => :plain,
     :enable_starttls_auto => true
   }
 end
