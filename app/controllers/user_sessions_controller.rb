d1c9a26080d1a6f8db6646beac52f348690413e5
hkwan <hkewnarny@gmail.com>
2012-10-21 10:49:46 -0700

    Changed logout to CAS Single Sign Out

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -19,7 +19,13 @@ class UserSessionsController < ApplicationController
     @user_session.destroy if @user_session
     @user_session = nil
     @current_user = nil
+    CASClient::Frameworks::Rails::Filter.logout(self)
+=begin
+    @user_session.destroy if @user_session
+    @user_session = nil
+    @current_user = nil
     flash[:notice] = "Logged out successfully"
     redirect_to home_path
+=end
   end
 end
