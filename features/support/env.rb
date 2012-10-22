878599a84b0667a70820b720b84c6e36bbd13f1f
Jeff Tsui <tsui.jeff@gmail.com>
2012-10-21 23:14:12 -0700

    add gem simplecov

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,8 +3,9 @@
 # newer version of cucumber-rails. Consider adding your own code to a new file 
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
-
+require 'simplecov'
 require 'cucumber/rails'
+SimpleCov.start 'rails'
 
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
