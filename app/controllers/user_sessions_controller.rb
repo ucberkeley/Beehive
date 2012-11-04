fea8b241d1489424ad7d0fb9c7e8ff9cfc2b3e60
saasbook <saasbook@saasbook.(none)>
2012-11-03 22:20:21 -0700

    Fixing auth_hash nil bug

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,6 +5,7 @@ include CASControllerIncludes
 #before_filter :goto_home_unless_logged_in, :except => :destroy
 
   def new
+    session[:auth_hash] = request.env['omniauth.auth']
     session[:cas_user] = request.env['omniauth.auth'][:uid]
     if login_user!(User.find_by_login(session[:cas_user]))
       redirect_to request.referer || home_path
@@ -15,11 +16,6 @@ include CASControllerIncludes
       flash[:notice] = "There was a problem logging in.. please try again later, or contact us if the problem persists."
       flash[:notice] += @user_session.errors.inspect if Rails.env == 'development'
     end
-  end
-
-  def auth_hash
-    session[:auth_hash] = request.env['omniauth.auth']
-    request.env['omniauth.auth']
   end
 
   def destroy
