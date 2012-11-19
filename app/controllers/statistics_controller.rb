ce938fc5f3518298d850eac99fc510c6c5eb2388
hkwan <hkewnarny@gmail.com>
2012-11-18 19:21:51 -0800

    Added in basic application statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,6 +3,9 @@ class StatisticsController < ApplicationController
   before_filter :goto_home_unless_logged_in
 
   def index
-    
+    applics_this_month = Applic.where("created_at >= ?", Time.now.beginning_of_month) 
+    applics_in_last_month = Applic.where("created_at >= ?", Time.now - 1.month) 
+    @num_applics_this_month = applics_this_month.size
+    @num_applics_in_last_month = applics_in_last_month.size
   end
 end
