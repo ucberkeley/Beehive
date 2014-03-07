015d97a5874ed5cc7a97c25cb7a9ad6ad36b2c47
Max Lam <agnusmaximus@gmail.com>
2014-03-06 23:19:13 -0800

    Added all files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,13 +34,13 @@ ResearchMatch::Application.configure do
   ActionMailer::Base.delivery_method = !!(ENV['action_mailer'] =~ /1|true/i) ? :smtp : :test
   puts "INFO: ActionMailer ON" if ActionMailer::Base.delivery_method == :smtp
   
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
+  #config.after_initialize do
+  #  Bullet.enable = true
+  #  Bullet.alert = true
+  #  Bullet.bullet_logger = true
+  #  Bullet.console = true
     #  Bullet.growl = true
-    Bullet.rails_logger = true
-    Bullet.add_footer = true
-  end
+  #  Bullet.rails_logger = true
+  #  Bullet.add_footer = true
+  #end
 end
