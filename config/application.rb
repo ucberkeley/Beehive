0b150d1f9632185e5fed0b3ab1ae53120a6f7720
Max Lam <agnusmaximus@gmail.com>
2014-03-07 03:31:44 -0800

    Fixed some exception notification gem deprecation

--- config/application.rb
+++ config/application.rb
@@ -48,6 +48,6 @@ module ResearchMatch
 
     # Prevent initializing application and connecting to the database
     config.assets.precompile += ['*.js']
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
