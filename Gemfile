d987b0bc5eb53be8093ae9bd1666625c557d342a
Allen Zhu <allen.l.zhu@gmail.com>
2014-03-07 00:33:20 -0800

    Enable asset pipeline

--- Gemfile
+++ Gemfile
@@ -59,6 +59,13 @@ group :test do
   gem 'simplecov'
 end
 
+group :assets do
+  gem 'therubyracer'
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
 
 ################################################################################
 
--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,13 @@ module ResearchMatch
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
