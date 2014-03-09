8df5c8976985ad18602768481c93795623809038
Max Lam <agnusmaximus@gmail.com>
2014-03-09 01:18:15 -0800

    Relaunching heroku2

--- config/application.rb
+++ config/application.rb
@@ -46,7 +46,7 @@ module ResearchMatch
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
 
