565c8acd40d9fa7035b261ed744c618bf5d324f2
themetric <marklinstop@gmail.com>
2014-05-01 15:07:10 -0700

    migrate and fix typo

--- app/models/job.rb
+++ app/models/job.rb
@@ -19,9 +19,6 @@ class Job < ActiveRecord::Base
   #   end_date            : datetime 
   #   compensation        : integer 
   #   status              : integer 
-  #   primary_contact_id  : integer 
-  #   project_type        : integer 
-  #   open                : boolean 
   # =======================
 
   include AttribsHelper
