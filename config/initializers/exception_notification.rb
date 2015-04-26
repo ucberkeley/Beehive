e03d30f119c87d747e8b9b7edac28a681b8c25b0
allenz <allenzhu@berkeley.edu>
2015-04-26 15:04:21 -0700

    Upgrade exception config

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -7,11 +7,11 @@ if (email=ENV['NOTIFIER_EMAIL']).blank?
     raise ArgumentError.new "No exception notification email set. See #{__FILE__}"
   end
 else
-  ResearchMatch::Application.config.middleware.use(ExceptionNotifier, {
-    :email_prefix         => '[RM Notifier] ',
-    :sender_address       => "ResearchMatch Notifier <#{email}>",
-    :exception_recipients => [ email ],
-    :sections             => %w( more ) + ExceptionNotifier::Notifier.default_sections
-  })
+  ResearchMatch::Application.config.middleware.use ExceptionNotification::Rack,
+    :email => {
+      :email_prefix         => '[RM Notifier] ',
+      :sender_address       => "ResearchMatch Notifier <#{email}>",
+      :exception_recipients => email
+    }
   ExceptionNotifier::Notifier.prepend_view_path File.join(Rails.root, 'app', 'views')
 end
