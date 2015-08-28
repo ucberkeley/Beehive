4b4f3cf9633309b398cc63a518298f343c0a5f61
huangshan108 <shanhuang108@gmail.com>
2015-08-27 23:54:14 -0700

    lots of stuffs cleaned up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,41 +47,6 @@ function textChange(elementID, defaultText)
    }
 }
 
-function menu_bar_one(){
-	//Menu Bar Button Hover
-	$("menu_bar_01").src = "../images/menu_bar_fade_01.png";
-	$("menu_bar_01").observe('mouseout', function() {
-	  this.src = "../images/menu_bar_fade_01.png";
-	});
-	$("menu_bar_01").observe('mouseover', function() {
-	  this.src = "../images/menu_bar_01.png";
-	});
-
-}
-
-function menu_bar_two(){
-	//Menu Bar Button Hover
-	$("menu_bar_02").src = "../images/menu_bar_fade_02.png";
-	$("menu_bar_02").observe('mouseout', function() {
-	  this.src = "../images/menu_bar_fade_02.png";
-	});
-	$("menu_bar_02").observe('mouseover', function() {
-	  this.src = "../images/menu_bar_02.png";
-	});
-
-}
-
-function menu_bar_three(){
-	//Menu Bar Button Hover
-	$("menu_bar_03").src = "../images/menu_bar_fade_03.png";
-	$("menu_bar_03").observe('mouseout', function() {
-	  this.src = "../images/menu_bar_fade_03.png";
-	});
-	$("menu_bar_03").observe('mouseover', function() {
-	  this.src = "../images/menu_bar_03.png";
-	});
-}
-
 /* users/new signup page sliding effects */
 
 function user_signup_student_fields() {
@@ -95,12 +60,6 @@ function user_signup_faculty_fields() {
     $('#fields_student_below_password').slideUp();
     $('#fields_faculty').slideDown();
 }
-
-
-/* autocomplete endpoint GET requests */
-// function get_categories() {
-//     return $.getJSON('/research/categories/json');
-// }
 
 $(document).ready(function(){
   setupExternalLinks();
