4f37c18823749953f436db14868aad7cfb734402
huangshan108 <shanhuang108@gmail.com>
2015-09-01 00:16:54 -0700

    dashboard table updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
 //= require bootstrap
 //= require bootstrap-material-design
 function setupExternalLinks() {
--- app/models/user.rb
+++ app/models/user.rb
@@ -78,7 +78,7 @@ class User < ActiveRecord::Base
 
   # Name
   validates_presence_of     :name
-  validates_length_of       :name,     :within => 0..100  
+  validates_length_of       :name,     :within => 0..100
   # ignore validation for now
   # validates_format_of       :name,     :with => /\A[A-Za-z\-_ \.']+\z/
 
