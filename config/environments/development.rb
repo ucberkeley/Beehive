a9234a3a4081bb70e410425369477adf007cd781
allenz <allenzhu@berkeley.edu>
2016-03-07 17:55:18 -0800

    Attempt to send email from gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,16 @@ ResearchMatch::Application.configure do
 
   # ActionMailer
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { :host => "researchmatch-dev.herokuapp.com" }
 end
 
